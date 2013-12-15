bbde9855cab40ec3cefb5d4ef12cf27c57106123
chrisfinch <chrisfinchy@gmail.com>
2013-12-15 11:00:03 +0000

    restart command

--- nodejs.sh
+++ nodejs.sh
@@ -16,6 +16,12 @@ stop)
 	killall $NODE
 	;;
 
+restart)
+	killall $NODE
+	echo "starting node: $NODE $SERVER_JS_FILE"
+	sudo -u $USER $NODE $SERVER_JS_FILE > $OUT 2>$OUT &
+	;;
+
 *)
 	echo "usage: $0 (start|stop)"
 esac
