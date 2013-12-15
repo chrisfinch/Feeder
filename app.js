46de9019ba3b8499e671f5cea979db71c931f089
chrisfinch <chrisfinchy@gmail.com>
2013-12-15 10:59:06 +0000

    test

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.get('/led', function (req, res) {
+
+	res.render('index', { title: 'Done!' });
+
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
