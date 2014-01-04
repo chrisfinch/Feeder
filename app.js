08f7c3d12d5e987fa06e5549adba206393729c93
chrisfinch <chrisfinchy@gmail.com>
2014-01-04 18:54:48 +0000

    changes

--- app.js
+++ app.js
@@ -71,6 +71,32 @@ var pinOff = function (pin) {
 	});
 };
 
+var getDateTime = function () {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    // return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+    return hour + ":" + min + ":" + sec;
+
+};
+
 var turnDuration = 500;
 
 app.get('/feed', function (req, res) {
@@ -84,10 +110,7 @@ app.get('/feed', function (req, res) {
 		pinOff(7);
 		res.send(200);
 
-		var now = new Date();
-
-
-		T.post('statuses/update', { status: 'I just fed the cats at ' + now.getHours()+1+':'+now.getMinutes()+1 }, function(err, reply) {
+		T.post('statuses/update', { status: 'I just fed the cats at ' + getDateTime() }, function(err, reply) {
 			if (!err) {
 				res.send(200);
 			} else {
