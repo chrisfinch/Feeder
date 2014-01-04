ef0c0aa71836fbed792742e326ff8862f6dec458
chrisfinch <chrisfinchy@gmail.com>
2014-01-04 20:28:26 +0000

    changes

--- app.js
+++ app.js
@@ -56,9 +56,35 @@ app.get('/tweet', function (req, res) {
 	});
 });
 
+function copyFile(source, target, cb) {
+  var cbCalled = false;
+
+  var rd = fs.createReadStream(source);
+  rd.on("error", function(err) {
+    done(err);
+  });
+  var wr = fs.createWriteStream(target);
+  wr.on("error", function(err) {
+    done(err);
+  });
+  wr.on("close", function(ex) {
+    done();
+  });
+  rd.pipe(wr);
+
+  function done(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
+    }
+  }
+}
+
 var updateImage = setInterval(function () {
 
-	fs.createReadStream('/tmp/motion/lastsnap.jpg').pipe(fs.createWriteStream(path.join(__dirname, 'public', 'lastsnap.jpg')));
+	copyFile('/tmp/motion/lastsnap.jpg', path.join(__dirname, 'public', 'lastsnap.jpg'), function (res) {
+		console.log(res);
+	});
 
 }, 10000);
 
